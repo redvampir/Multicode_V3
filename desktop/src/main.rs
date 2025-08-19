329893b4b41a975ccd9f7f534352f34a1ab9b056
redvampir <93953323+redvampir@users.noreply.github.com>
2025-08-19 17:39:21 +0300

    feat(desktop): add project and file CLI args

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,6 +1,23 @@
-mod modal;
 mod app;
+mod modal;
+
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(long)]
+    file: Option<PathBuf>,
+    #[arg(long)]
+    project: Option<PathBuf>,
+}
 
 pub fn main() -> iced::Result {
-    app::run()
+    let args = Args::parse();
+    let path = if let Some(file) = args.file {
+        file.parent().map(|p| p.to_path_buf())
+    } else {
+        args.project
+    };
+    app::run(path)
 }
