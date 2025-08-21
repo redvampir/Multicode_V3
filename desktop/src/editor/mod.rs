8e3c8eb659783653f34ae6a3bbf4083bb719ba36
redvampir <93953323+redvampir@users.noreply.github.com>
2025-08-21 19:45:22 +0300

    Validate meta comments

--- desktop/src/editor/mod.rs
+++ desktop/src/editor/mod.rs
@@ -1,10 +1,11 @@
-pub mod code_editor;
 pub mod autocomplete;
+pub mod code_editor;
+pub mod meta_integration;
 pub mod syntax_highlighter;
 
+pub use autocomplete::{suggestions, AutocompleteState, Suggestion};
 pub use code_editor::CodeEditor;
 pub use syntax_highlighter::THEME_SET;
-pub use autocomplete::{AutocompleteState, Suggestion, suggestions};
 
 #[cfg(test)]
 mod code_editor_tests;
